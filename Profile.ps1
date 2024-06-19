e791fdd8238681e17f184d56d956d21c77b366ba
Nicolas Stirnemann <nstirnemann59@gmail.com>
2024-06-18 22:42:49 -0300

    Correct config for multiple platforms

---
+++ Profile.ps1
@@ -0,0 +1,20 @@
+# STARSHIP
+
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+## GENERAL FUNCTIONS
+function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
+
+function l { 
+  lsd -la
+}
+
